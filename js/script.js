f2ae28a981adc609cf400e0ab1655f4c1a63be22
Ridwan4D <ridwanul161@gmail.com>
2025-02-16 15:34:47 +0600

    popular section created

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const tabs = document.querySelectorAll(".tab_btn");
+const all_content = document.querySelectorAll(".tab_content");
+let line = document.querySelector(".line");
+const counts = document.querySelectorAll(".futura_bottom_info");
+const speed = 100;
+
+tabs.forEach((tab, idx) => {
+    tab.addEventListener("click", (e) => {
+        tabs.forEach((tab) => {
+            tab.classList.remove("active");
+        });
+        tab.classList.add("active");
+        line.style.width = e.target.offsetWidth + "px";
+        line.style.left = e.target.offsetLeft + "px";
+
+        all_content.forEach((content) => {
+            content.classList.remove("active");
+        });
+        all_content[idx].classList.add("active");
+    });
+});
+
+const observer = new IntersectionObserver(
+    (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const counter = entry.target;
+                function updateData() {
+                    const target = Number(counter.getAttribute("data-target"));
+                    let count = 0;
+                    const increment = target / speed;
+
+                    const interval = setInterval(() => {
+                        count += increment;
+                        if (count >= target) {
+                            counter.innerText = target + " +";
+                            clearInterval(interval);
+                        } else {
+                            counter.innerText = Math.floor(count) + " +";
+                        }
+                    }, 8);
+                }
+
+                updateData();
+                observer.unobserve(counter);
+            }
+        });
+    },
+    { threshold: 0.5 }
+);
+
+counts.forEach((counter) => {
+    observer.observe(counter);
+});
